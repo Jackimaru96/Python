24afc4c1745181d0e8c360d0ca302f2b988ae3a6
Jackimaru96 <jackchenyujie.96@gmail.com>
2023-04-28 17:00:51 +0800

    Add files for HelloWorld and Variables

---
+++ Variables/datatypes.py
@@ -0,0 +1,69 @@
+# ##################  Data Types ##################  
+
+# #### String 🧵 ####
+# # When we create Strings, we need to create them with double quotes ("") around the string we want
+# # This is because this is a string of characters
+
+# # Getting characters from string using bracket notation
+# print("The below prints out the first character")
+# print("Hello"[0])
+
+# # Getting characters from string using bracket notation
+# print("The below prints out the first character")
+# print("Hello"[0])
+
+# print()
+# name = "Tom Tan"
+# print("We can also splice and slice our string to get a substring")
+
+# # Get character @ 0, 1, 2 index
+# first_name = print(name[0:3])
+# # If we do not put an end_index, it will take all the characters from the start_index
+# # last character of the string
+# last_name = print(name[4:])
+
+# # String concatenation
+# print()
+# first_name = "Tom"
+# last_name = "Tan"
+# print(first_name + " " + last_name)
+# print()
+
+# ###### Integer 🔢 ######
+# age = 15
+# print(number)
+
+# ###### Float 5️⃣⚫ ######
+# decimal_number = 12.15
+# print(decimal_number)
+
+
+# ###### Type Error & Type Conversion ########
+# age = 5
+# print("My age is " + str(age))
+
+# # ######## str -> int
+# print("Recall that input() will take in user input, and this user input is of String data type")
+# number_str = input("Please input your number: ")
+
+# # We need to convert the number which is currently a string into int/float data type
+# number_int = int(number_str)
+# print(number_int + 5)
+
+# # We can also immediate convert the user's input to int using function nesting
+# number = int(input("Please input your number (This converts to int type straight away): "))
+# print(number + 5)
+# print()
+
+
+# ######## int -> str
+# print("We can convert integer into string")
+# age_number = 20
+# print("My age is " + str(age_number))
+
+# number = 2.99999
+# print(int(number) + 5)
+
+# # We can also declare a new variable to store the age that is of type String
+# age_number_str = str(age_number)
+# print("My age is " + age_number_str)
